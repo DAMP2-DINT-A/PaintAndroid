92f40ff7e609201b31283c11d29352dcdbe1ad70
daniel <daniel_sephirot@hotmail.com>
2017-01-22 20:38:05 +0100

    commit add paint

--- app/src/main/java/com/example/asus/paint/MainActivity.java
+++ app/src/main/java/com/example/asus/paint/MainActivity.java
@@ -18,7 +18,7 @@ public class MainActivity extends AppCompatActivity {
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.dibujar_en_pantalla, menu);
+        getMenuInflater().inflate(R.menu.dibujar, menu);
         return true;
     }
 
